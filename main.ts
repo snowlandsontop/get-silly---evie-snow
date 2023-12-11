079d38528259727ad2b7db69e3ed9dfc4885932e
snowlandsontop <152302239+snowlandsontop@users.noreply.github.com>
2023-12-11 08:48:47 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Confused)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Sad)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.Silly)
+})
+basic.forever(function () {
+	
+})
